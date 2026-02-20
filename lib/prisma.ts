889438820e1c30f99129a5983b079efe46f9e0ed
Martin Bruun <martinbruun1995@gmail.com>
2026-02-21 00:33:33 +0100

    First commit

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,8 @@
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import { PrismaClient } from "@prisma/client";
+
+const databaseUrl = process.env.DATABASE_URL ?? "file:./prisma/dev.db";
+const adapter = new PrismaBetterSqlite3({ url: databaseUrl });
 
 declare global {
   // eslint-disable-next-line no-var
@@ -8,6 +12,7 @@ declare global {
 export const prisma =
   global.prisma ??
   new PrismaClient({
+    adapter,
     log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"]
   });
 
